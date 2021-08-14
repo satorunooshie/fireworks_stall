aa663baa2ac1e4cf81bd27897d73675df2f86823
Misato Watanabe <55613353+tsubame-misa@users.noreply.github.com>
2021-08-14 19:56:06 +0900

    スコアのポップアップ表示 (#16)
    * スコアのポップアップ表示#
    
    * 色と確率の渡し

--- fireworks-app/src/pages/Setup.js
+++ fireworks-app/src/pages/Setup.js
@@ -1,21 +1,50 @@
 import React from "react";
+import { useState } from "react";
 import P5Wrapper from "react-p5-wrapper";
 import testSketch from "../components/fireworks";
 import "./Setup.css";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function Setup() {
+  const [fin, setFin] = useState(false);
+  const { color } = useParams();
+
+  setTimeout(function () {
+    console.log("time out");
+    setFin(true);
+    //多分10秒くらいがちょうどいい
+  }, 5000);
+
   return (
     <div className="container">
       <div>
-        <P5Wrapper sketch={testSketch} />
+        <P5Wrapper
+          sketch={testSketch}
+          color={`#${color}`}
+          p={Math.random(0, 1)}
+        />
       </div>
 
-      <div className="button-block">
-        <button>
-          <Link to={`/score`}>スコアを見る</Link>
-        </button>
-      </div>
+      {fin ? (
+        <div>
+          <div style={{ marginTop: "40vh", height: "200px" }}>
+            <div className="score">
+              <div>
+                <div>得点</div>
+              </div>
+              <button>
+                <Link to={`/score`}>ランキング</Link>
+              </button>
+              <button>
+                <Link to={`/make`}>もう一回やる</Link>
+              </button>
+              <button>twitterで共有</button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        []
+      )}
     </div>
   );
 }
