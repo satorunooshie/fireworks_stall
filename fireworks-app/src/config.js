85060aef7bac581e247857f24f0f53751cf10319
unibon4141 <52618450+unibon4141@users.noreply.github.com>
2021-08-14 19:47:04 +0900

    Feature start page (#17)
    * commit
    
    * commit

---
+++ fireworks-app/src/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+// import "firebase/auth";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB4tIEtJMtbiKFPy5Dkk55C3pZNgHiY_T0",
+  authDomain: "fireworks-test-5cb93.firebaseapp.com",
+  projectId: "fireworks-test-5cb93",
+  storageBucket: "fireworks-test-5cb93.appspot.com",
+  messagingSenderId: "797436327373",
+  appId: "1:797436327373:web:a40be576e3bf8475f34084",
+});
+// export const provider = new firebase.auth.TwitterAuthProvider();
+export default firebase;
+// Your web app's Firebase configuration
--- fireworks-app/src/pages/Login.js
+++ fireworks-app/src/pages/Login.js
@@ -1,4 +1,50 @@
+import firebase from "../config";
 function Login() {
+  function loginWithTwitter() {
+    var provider = new firebase.auth.TwitterAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+
+        // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+        // You can use these server side with your app's credentials to access the Twitter API.
+        var token = credential.accessToken;
+        var secret = credential.secret;
+
+        // The signed-in user info.
+        var user = result.user;
+        var usename = user.displayName;
+        localStorage.setItem("username", usename);
+        localStorage.setItem("Authorization", "Bearer " + user.bc.Aa);
+        console.log(user.bc.Aa);
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  }
+  function logoutHandler() {
+    firebase
+      .auth()
+      .signOut()
+      .then(function () {
+        // Sign-out successful.
+      })
+      .catch(function (error) {
+        // An error happened.
+      });
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +59,8 @@ function Login() {
         >
           Learn React
         </a>
+        <button onClick={loginWithTwitter}>login</button>
+        <button onClick={logoutHandler}>logout</button>
       </header>
     </div>
   );
