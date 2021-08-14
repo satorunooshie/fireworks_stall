8c82ac42ab41abd0e24038a3793f0372a17626e5
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-08-14 17:01:32 +0900

    スコア画面への遷移2

--- fireworks-app/src/pages/Setup.js
+++ fireworks-app/src/pages/Setup.js
@@ -1,11 +1,21 @@
 import React from "react";
 import P5Wrapper from "react-p5-wrapper";
 import testSketch from "../components/fireworks";
+import "./Setup.css";
+import { Link } from "react-router-dom";
 
 function Setup() {
   return (
-    <div>
-      <P5Wrapper sketch={testSketch} />
+    <div className="container">
+      <div>
+        <P5Wrapper sketch={testSketch} />
+      </div>
+
+      <div className="button-block">
+        <button>
+          <Link to={`/score`}>スコアを見る</Link>
+        </button>
+      </div>
     </div>
   );
 }
