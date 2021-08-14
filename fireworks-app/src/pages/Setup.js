488244a932fbac5820a45bbf49cd35371a0f92f0
Misato Watanabe <55613353+tsubame-misa@users.noreply.github.com>
2021-08-14 16:43:53 +0900

    打ち上げひとまず成功 (#13)
    * p5jsの組み込み
    
    * 花火打ち上げ成功

--- fireworks-app/src/pages/Setup.js
+++ fireworks-app/src/pages/Setup.js
@@ -1,7 +1,11 @@
+import React from "react";
+import P5Wrapper from "react-p5-wrapper";
+import testSketch from "../components/fireworks";
+
 function Setup() {
   return (
-    <div className="App">
-      <header className="App-header">set up</header>
+    <div>
+      <P5Wrapper sketch={testSketch} />
     </div>
   );
 }
