50888243625ff24333258f48d4bd71ef25f1847e
satorunooshie <64164948+satorunooshie@users.noreply.github.com>
2021-08-14 21:27:33 +0900

    game finish (#19)

--- pkg/interfaces/handler/user_handler.go
+++ pkg/interfaces/handler/user_handler.go
@@ -7,13 +7,14 @@ import (
 	"net/http"
 
 	"github.com/satorunooshie/fireworks_stall/pkg/dcontext"
+	"github.com/satorunooshie/fireworks_stall/pkg/domain/model"
 	userU "github.com/satorunooshie/fireworks_stall/pkg/usecase"
 )
 
 type UserHandler interface {
 	HandleGetLevel() http.HandlerFunc
 	HandleInsert() http.HandlerFunc
-	HandleUpdate() http.HandlerFunc
+	HandleSaveScore() http.HandlerFunc
 	HandleDelete() http.HandlerFunc
 }
 
@@ -65,14 +66,60 @@ func (userH *userHandler) HandleGetLevel() http.HandlerFunc {
 }
 
 // HandleInsert ...
-func (userH *userHandler) HandleInsert() http.HandlerFunc {
+func (userH *userHandler) HandleSaveScore() http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
-		panic("do something")
+		var req userHandleSaveScoreRequest
+		if err := json.NewDecoder(request.Body).Decode(&req); err != nil {
+			log.Printf("[WARN] json decode: %v", err)
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+			return
+		}
+		if req.Score <= 0 {
+			log.Printf("[INFO] req.Score <= 0: %v", req.Score)
+			http.Error(writer, "request invalid", http.StatusBadRequest)
+			return
+		}
+		ctx := request.Context()
+		uid := dcontext.GetUIDFromContext(ctx)
+
+		user, err := userH.userUseCase.SelectUser(ctx, uid)
+		if err != nil {
+			log.Printf("[ERROR] select user: %v", err.Error())
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if user == nil {
+			log.Print("[ERROR] user not found")
+			http.Error(writer, "user not found", http.StatusBadRequest)
+			return
+		}
+		if req.Score <= user.Score {
+			writer.WriteHeader(http.StatusOK)
+			writer.Header().Set("Content-Type", "application/json")
+			return
+		}
+		// 条件分岐
+		level := user.Level
+		if l := calculateLevel(req.Score); user.Level < l {
+			level = l
+		}
+		ent := &model.User{
+			UID:   uid,
+			Score: req.Score,
+			Level: level,
+		}
+		if err := userH.userUseCase.Update(ctx, ent); err != nil {
+			log.Printf("[ERROR] update user: %v", err.Error())
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		writer.WriteHeader(http.StatusOK)
+		writer.Header().Set("Content-Type", "application/json")
 	}
 }
 
-// HandleUpdate ...
-func (userH *userHandler) HandleUpdate() http.HandlerFunc {
+// HandleInsert ...
+func (userH *userHandler) HandleInsert() http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		panic("do something")
 	}
@@ -99,4 +146,20 @@ type UserResponse struct {
 
 type userHandleGetLevelResponse struct {
 	Level int32 `json:"level"`
+}
+
+type userHandleSaveScoreRequest struct {
+	Score int32 `json:"score"`
+}
+
+func calculateLevel(score int32) int32 {
+	switch {
+	case score > 500:
+		return 3
+	case score > 300:
+		return 2
+	default:
+		return 1
+
+	}
 }
