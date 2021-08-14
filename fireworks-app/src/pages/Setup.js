0a92f8cda5ea0ce4ca630f0cdd6a3852fc940db1
Misato Watanabe <55613353+tsubame-misa@users.noreply.github.com>
2021-08-14 22:25:47 +0900

    スコアカードの調整 (#22)

--- fireworks-app/src/pages/Setup.js
+++ fireworks-app/src/pages/Setup.js
@@ -8,7 +8,7 @@ import { Link, useParams } from "react-router-dom";
 function Setup() {
   const [fin, setFin] = useState(false);
   const { color } = useParams();
-
+  const p = 0.5;
   setTimeout(function () {
     console.log("time out");
     //setFin(true);
@@ -18,27 +18,30 @@ function Setup() {
   return (
     <div className="container">
       <div>
-        <P5Wrapper
-          sketch={testSketch}
-          color={`#${color}`}
-          p={Math.random(0, 1)}
-        />
+        <P5Wrapper sketch={testSketch} color={`#${color}`} p={p} />
       </div>
 
       {fin ? (
         <div>
-          <div style={{ marginTop: "40vh", height: "200px" }}>
-            <div className="score">
+          <div className="score-card">
+            <div className="score center">
               <div>
-                <div>得点</div>
+                <div style={{ textAlign: "center" }}>
+                  <div style={{ fontSize: "1.75rem" }}>けっかはっぴょう</div>
+                  <p style={{ fontSize: "3rem", margin: "0.5rem" }}>
+                    <b>{Math.round(300 * p)}</b>m
+                  </p>
+                </div>
+                <div className="button-block">
+                  <button>
+                    <Link to={`/score`}>ランキング</Link>
+                  </button>
+                  <button>
+                    <Link to={`/make`}>もう一回やる</Link>
+                  </button>
+                  <button>twitterで共有</button>
+                </div>
               </div>
-              <button>
-                <Link to={`/score`}>ランキング</Link>
-              </button>
-              <button>
-                <Link to={`/make`}>もう一回やる</Link>
-              </button>
-              <button>twitterで共有</button>
             </div>
           </div>
         </div>
