622be118623d43ffb02264fcfdbd054e435c866f
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-08-15 11:30:43 +0900

    update

--- fireworks-app/src/pages/Setup.js
+++ fireworks-app/src/pages/Setup.js
@@ -44,11 +44,16 @@ function Setup() {
     f();
   }, [color]);
 
-  const twitteTxt = `http://twitter.com/share?url=https://firework-stall.netlify.app&text=【あなたが打ち上げた花火は${
+  /*const twitteTxt = `http://twitter.com/share?url=https://firework-stall.netlify.app&text=【あなたが打ち上げた花火は${
     300 * p
   }m%まで飛びました！
    %0▼みんなも花火を作って打ち上げよう &hashtags=わくわく花火屋さん&count=horizontal&lang=ja`;
+*/
 
+  const twitteTxt = `http://twitter.com/share?url=https://firework-stall.netlify.app&text=あなたが打ち上げた花火は${
+    300 * p
+  }m%まで飛びました！
+   %0▼みんなも花火を作って打ち上げよう!&hashtags=奇声JUDGE&count=horizontal&lang=ja`;
   return (
     <div className="container">
       <div>
