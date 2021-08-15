3e5b531376e30fa5224da1b620acf907022be4c5
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-08-15 12:07:20 +0900

    fiiiiiix

--- pkg/interfaces/middleware/cors.go
+++ pkg/interfaces/middleware/cors.go
@@ -28,7 +28,6 @@ func httpMethod(next http.HandlerFunc, method string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Origin", "*")
 		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Accept, Origin, Authorization, username")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		// if r.Method == http.MethodOptions {
 		// 	return
